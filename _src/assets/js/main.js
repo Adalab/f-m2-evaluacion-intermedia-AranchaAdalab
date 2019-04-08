dbc84efdb28eddb00d6d7bdeea8e38e883f45688
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-08 09:43:33 +0200

    first attempt

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('>> Ready :)');
+const input = document.querySelector('.input');
+const button = document.querySelector('.btn');
+const feedback = document.querySelector('.feedback');
+const counter = document.querySelector('.counter');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  
+  console.log('> ' + getRandomNumber(100));
+
+function showNumber() {
+    console.log (input.value);
+    return input.value;
+}
+
+button.addEventListener('click', showNumber);
+
+if (showNumber () < getRandomNumber()) {
+    feedback.innerHTML = 'demasiado bajo';
+} else if (showNumber () > getRandomNumber()) {
+    feedback.innerHTML = 'demasiado alto';
+} else if (showNumber () === getRandomNumber()){
+    feedback.innerHTML = '¡HAS GANADO CAMPEONA!';
+}
+
+const numberInput = showNumber(input.value);
+const randomNumber = getRandomNumber ();
+
+if (numberInput < getRandomNumber()) {
+    feedback.innerHTML = 'demasiado bajo';
+}
