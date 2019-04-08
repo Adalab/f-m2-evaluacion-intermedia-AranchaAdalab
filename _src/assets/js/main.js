82445aafb95a13e883cec0b20dba5605a61b8cfd
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-08 10:21:23 +0200

    fix exercise

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,31 +4,29 @@ const input = document.querySelector('.input');
 const button = document.querySelector('.btn');
 const feedback = document.querySelector('.feedback');
 const counter = document.querySelector('.counter');
+const randomNumber = getRandomNumber (100);
+let i = 0;
+counter.innerHTML = i;
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-  }
-  
-  console.log('> ' + getRandomNumber(100));
+}
+console.log('>', randomNumber);
 
 function showNumber() {
-    console.log (input.value);
-    return input.value;
+    const numberInput = parseInt(input.value);
+    console.log (numberInput);
+    if (numberInput < randomNumber) {
+        feedback.innerHTML = 'demasiado bajo';
+    } else if (numberInput > randomNumber) {
+        feedback.innerHTML = 'demasiado alto';
+    } else if (numberInput === randomNumber){
+        feedback.innerHTML = '¡HAS GANADO CAMPEONA!';
+    }
+    i = i+1;
+    counter.innerHTML = i;
 }
+
 
 button.addEventListener('click', showNumber);
 
-if (showNumber () < getRandomNumber()) {
-    feedback.innerHTML = 'demasiado bajo';
-} else if (showNumber () > getRandomNumber()) {
-    feedback.innerHTML = 'demasiado alto';
-} else if (showNumber () === getRandomNumber()){
-    feedback.innerHTML = '¡HAS GANADO CAMPEONA!';
-}
-
-const numberInput = showNumber(input.value);
-const randomNumber = getRandomNumber ();
-
-if (numberInput < getRandomNumber()) {
-    feedback.innerHTML = 'demasiado bajo';
-}
