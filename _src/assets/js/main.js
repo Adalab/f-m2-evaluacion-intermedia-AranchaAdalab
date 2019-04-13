008190eefcd300e8f5163dc1260dc1a60cd86408
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-13 14:11:01 +0200

    add feedback for bigger and smaller numbers

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,6 +4,7 @@ const input = document.querySelector('.input');
 const button = document.querySelector('.btn');
 const feedback = document.querySelector('.feedback');
 const counter = document.querySelector('.counter');
+const popup = document.querySelector('.popup');
 const randomNumber = getRandomNumber (100);
 let i = 0;
 counter.innerHTML = i;
@@ -11,22 +12,31 @@ counter.innerHTML = i;
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
-console.log('>', randomNumber);
+// console.log('>', randomNumber);
 
 function showNumber() {
     const numberInput = parseInt(input.value);
     console.log (numberInput);
-    if (numberInput < randomNumber) {
-        feedback.innerHTML = 'demasiado bajo';
-    } else if (numberInput > randomNumber) {
-        feedback.innerHTML = 'demasiado alto';
+    if (numberInput < randomNumber && numberInput > 0) {
+        feedback.innerHTML = 'Demasiado bajo';
+    } else if (numberInput < randomNumber && numberInput < 0){
+        feedback.innerHTML = 'Los números negativos no molan';
+    } else if (numberInput > randomNumber && numberInput < 100) {
+        feedback.innerHTML = 'Demasiado alto';
+    } else if (numberInput > randomNumber && numberInput > 100){
+        feedback.innerHTML = 'Tres cifras no es buena idea';
     } else if (numberInput === randomNumber){
         feedback.innerHTML = '¡HAS GANADO CAMPEONA!';
-    }
+    } 
     i = i+1;
     counter.innerHTML = i;
 }
 
+function showPopup () {
+    if (input.value === '') {
+        popup.classList.remove('hidden');
+    }
+}
 
-button.addEventListener('click', showNumber);
+button.addEventListener('click', showPopup, showNumber);
 
