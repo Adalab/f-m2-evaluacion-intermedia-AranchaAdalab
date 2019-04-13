b8a4872dec373e05e08981a764fcd5a020ac1d94
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-13 19:32:55 +0200

    reset counter

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,7 +5,9 @@ const button = document.querySelector('.btn');
 const feedback = document.querySelector('.feedback');
 const counter = document.querySelector('.counter');
 const popup = document.querySelector('.popup');
+const reset = document.querySelector('.reset');
 const randomNumber = getRandomNumber (100);
+console.log(`-- ${randomNumber}`);
 let i = 0;
 counter.innerHTML = i;
 
@@ -29,7 +31,7 @@ function showNumber() {
     } else if (numberInput > randomNumber && numberInput < 100) {
         feedback.innerHTML = 'Demasiado alto';
     } else if (numberInput > randomNumber && numberInput > 100){
-        feedback.innerHTML = 'Tres cifras no es buena idea';
+        feedback.innerHTML = 'Más de dos cifras no es buena idea';
     } else if (numberInput === randomNumber){
         feedback.innerHTML = '¡HAS GANADO CAMPEONA!';
     } 
@@ -37,11 +39,22 @@ function showNumber() {
     counter.innerHTML = i;
 }
 
+
 button.addEventListener('click', showNumber);
 
-button.addEventListener('keyup',function(e){
-    if (e.keyCode === 13) {
-    showNumber;
-  }  
-});
+// input.addEventListener('keyup', function(event) {
+//     if (event.keyCode === 13) {
+//     event.showNumber();
+//     button.click();
+//   }  
+// });
 
+function resetAll() {
+    input.value = '';
+    i = 0;
+    counter.innerHTML = i;
+    feedback.innerHTML = feedback;
+    getRandomNumber (100); 
+}
+
+reset.addEventListener('click', resetAll);
