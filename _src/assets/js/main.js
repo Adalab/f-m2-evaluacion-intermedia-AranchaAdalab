862b390a3384ff1ee962b815c6764c08bc5e2164
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-18 12:17:24 +0200

    intro function and reset random number

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,55 +7,58 @@ const counter = document.querySelector('.counter');
 const popup = document.querySelector('.popup');
 const reset = document.querySelector('.reset');
 const span = 'Escribe un número y dale a <span class="attempt">Prueba</span>';
-const randomNumber = getRandomNumber (100);
-console.log(`-- ${randomNumber}`);
+let randomNumber = getRandomNumber (100);
+
 let i = 0;
 counter.innerHTML = i;
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
 
 function showNumber() {
-    const numberInput = parseInt(input.value);
-    if (input.value === '') {
-        popup.classList.remove('hidden');
-        i = i-1;
-        counter.innerHTML = i;
-    } else {
-        popup.classList.add('hidden');
-    }
-    if (numberInput < randomNumber && numberInput > 0) {
-        feedback.innerHTML = 'Demasiado bajo';
-    } else if (numberInput < randomNumber && numberInput < 0){
-        feedback.innerHTML = 'Los números negativos no molan';
-    } else if (numberInput > randomNumber && numberInput < 100) {
-        feedback.innerHTML = 'Demasiado alto';
-    } else if (numberInput > randomNumber && numberInput > 100){
-        feedback.innerHTML = 'Más de dos cifras no es buena idea';
-    } else if (numberInput === randomNumber){
-        feedback.innerHTML = '¡HAS GANADO CAMPEONA!';
-    } 
-    i = i+1;
+  const numberInput = parseInt(input.value);
+  if (input.value === '') {
+    popup.classList.remove('hidden');
+    i = i-1;
     counter.innerHTML = i;
+  } else {
+    popup.classList.add('hidden');
+  }
+  if (numberInput < randomNumber && numberInput > 0) {
+    feedback.innerHTML = 'Demasiado bajo';
+  } else if (numberInput < randomNumber && numberInput < 0){
+    feedback.innerHTML = 'Los números negativos no molan';
+  } else if (numberInput > randomNumber && numberInput < 100) {
+    feedback.innerHTML = 'Demasiado alto';
+  } else if (numberInput > randomNumber && numberInput > 100){
+    feedback.innerHTML = 'Más de dos cifras no es buena idea';
+  } else if (numberInput === randomNumber){
+    feedback.innerHTML = '¡HAS GANADO!';
+  }
+  i = i+1;
+  counter.innerHTML = i;
 }
 
+function myFunction(event) {
+  const x = event.keyCode;
+  if (x === 13) {
+    showNumber();
+  }
+}
 
 button.addEventListener('click', showNumber);
-
-// input.addEventListener('keyup', function(event) {
-//     if (event.keyCode === 13) {
-//     event.showNumber();
-//     button.click();
-//   }  
-// });
+document.addEventListener('keydown', myFunction);
 
 function resetAll() {
-    input.value = '';
-    i = 0;
-    counter.innerHTML = i;
-    feedback.innerHTML = span; 
-    // getRandomNumber (100);
+  input.value = '';
+  i = 0;
+  counter.innerHTML = i;
+  feedback.innerHTML = span;
+  randomNumber = getRandomNumber(100);
+  console.log(randomNumber);
 }
 
-reset.addEventListener('click', resetAll, getRandomNumber);
+reset.addEventListener('click', resetAll);
+
+console.log(`-- ${randomNumber}`);
